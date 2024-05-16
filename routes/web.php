7dd11bf87570d61c5932f6bf02a2c7d60489a697
Joan Paneque Domingo <joanpd0@gmail.com>
2024-05-16 22:25:27 +0200

    Cleaned web.php routes

--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,27 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 use App\Http\Controllers\SurgeriesController;
 use App\Http\Controllers\SurgeryTypesController;
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IndexController;
+
 
 Route::resources([
     'surgeries' => SurgeriesController::class,
     'surgeries.surgerytypes' => SurgeryTypesController::class,
 ]);
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [IndexController::class, 'index'])->name('index')
+    ->middleware(['auth']);
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::resource('profile', ProfileController::class)
+    ->only(['edit', 'update', 'destroy'])
+    ->middleware(['auth']);
 
 require __DIR__.'/auth.php';
